0273b803311a2abdd6d7550b1a084db973f15f1f
Cailloux Rocher <cailloux66>
2025-05-12 22:02:10 +0200

    ajout des boutons back et restart dans katarenga

--- Katarenga/Katarenga_ui.py
+++ Katarenga/Katarenga_ui.py
@@ -7,7 +7,7 @@ from Board_editor import Board_ui
 from Player.Player import Player
 
 class Katarenga_ui:
-    def __init__(self, board_obj, board_ui_obj,katarenga, width=640, height=640, title="Katarenga Board"):
+    def __init__(self, board_obj, board_ui_obj, katarenga, width=640, height=640, title="Katarenga Board"):
         pygame.init()
         self.__width = width
         self.__height = height
@@ -21,29 +21,56 @@ class Katarenga_ui:
         self.cell_size = 60
         self.copied_value = None
         self.katarenga = katarenga
+        self.board_obj = board_obj
+        
+        # Buttons setup
+        self.button_font = pygame.font.SysFont(None, 36)
+        self.back_button_rect = pygame.Rect(self.__width // 2 - 130, self.__height - 60, 120, 40)
+        self.restart_button_rect = pygame.Rect(self.__width // 2 + 10, self.__height - 60, 120, 40)
+        
+        
 
     def run(self):
         self.katarenga.place_pawn(self.board)
+        
+        
+        
         while self.running:
             self.handle_events()
             self.update()
             self.gameloop()
             self.draw()
             self.board_ui.draw_all_corners(self.__screen)
+            
+            # Draw buttons
+            self.draw_buttons()
+            
             pygame.display.flip()
             self.clock.tick(60)
-        pygame.quit()
-        sys.exit()
+        return  
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+                pygame.quit()
+                sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 self.handle_click(event.pos)
 
     def handle_click(self, position):
         x, y = position
+        
+        #click on the back button
+        if self.back_button_rect.collidepoint(x, y):
+            self.running = False
+            return
+            
+        # same with the restart button
+        if self.restart_button_rect.collidepoint(x, y):
+            self.restart_game()
+            return
+            
         offset_x = (self.__width - self.cell_size * 8) // 2
         offset_y = (self.__height - self.cell_size * 8) // 2
 
@@ -62,6 +89,9 @@ class Katarenga_ui:
             self.board[row][col] = color_code * 10 + player_code
             self.update()
             
+    def restart_game(self):# A completer !!!!!!!!!!!!!!!!!!!!!!!!!!!
+        print("Game restarted!")
+        
     def gameloop(self):
         pass
 
@@ -92,6 +122,19 @@ class Katarenga_ui:
                     self.draw_text(str(player_code), rect)
                     pygame.draw.circle(self.__screen, (0, 0, 0), rect.center, self.cell_size // 2 - 5)
                     
+    def draw_buttons(self):
+        # Draw "Back" button
+        pygame.draw.rect(self.__screen, (70, 70, 70), self.back_button_rect)
+        pygame.draw.rect(self.__screen, (255, 255, 255), self.back_button_rect, 2)
+        back_text = self.button_font.render("Back", True, (255, 255, 255))
+        self.__screen.blit(back_text, back_text.get_rect(center=self.back_button_rect.center))
+        
+        # Draw "Restart" button
+        pygame.draw.rect(self.__screen, (70, 70, 70), self.restart_button_rect)
+        pygame.draw.rect(self.__screen, (255, 255, 255), self.restart_button_rect, 2)
+        restart_text = self.button_font.render("Restart", True, (255, 255, 255))
+        self.__screen.blit(restart_text, restart_text.get_rect(center=self.restart_button_rect.center))
+                    
     def draw_text(self, text, rect):
         font = pygame.font.SysFont(None, 24)
         txt_surface = font.render(text, True, (255, 255, 255))
@@ -103,6 +146,6 @@ if __name__ == "__main__":
     board_obj = Board()
     board_ui_obj = Board_ui()
     katarenga = Katarenga()
-    app = Katarenga_ui(board_obj,board_ui_obj,katarenga)
+    app = Katarenga_ui(board_obj, board_ui_obj, katarenga)
     player = Player()
     app.run()
